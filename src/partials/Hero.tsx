edd90cec80e95f849de43153b895835f436c9822
Tay Duncan <Tay.Duncan@autotrader.co.uk>
2023-07-16 07:42:42 +0100

    fix: change name in hero

--- src/partials/Hero.tsx
+++ src/partials/Hero.tsx
@@ -10,7 +10,7 @@ const Hero = () => (
     <HeroAvatar
       title={
         <>
-          Hi there, I'm <GradientText>Ixartz</GradientText> 👋
+          Hi there, I'm <GradientText>Tay</GradientText> 👋
         </>
       }
       description={
