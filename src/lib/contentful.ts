149ba940f5f8d6b8214787ecc95076299fea297a
Tay Duncan <Tay.Duncan@autotrader.co.uk>
2023-07-18 19:42:25 +0100

    fix: wrap contentful logic in conditional

--- src/lib/contentful.ts
+++ src/lib/contentful.ts
@@ -1,9 +1,11 @@
 import contentful from 'contentful';
 
-export const contentfulClient = contentful.createClient({
-  space: import.meta.env.CONTENTFUL_SPACE_ID,
-  accessToken: import.meta.env.DEV
-    ? import.meta.env.CONTENTFUL_PREVIEW_TOKEN
-    : import.meta.env.CONTENTFUL_DELIVERY_TOKEN,
-  host: import.meta.env.DEV ? 'preview.contentful.com' : 'cdn.contentful.com',
-});
+export const contentfulClient =
+  import.meta.env.CONTENTFUL_SPACE_ID ??
+  contentful.createClient({
+    space: import.meta.env.CONTENTFUL_SPACE_ID,
+    accessToken: import.meta.env.DEV
+      ? import.meta.env.CONTENTFUL_PREVIEW_TOKEN
+      : import.meta.env.CONTENTFUL_DELIVERY_TOKEN,
+    host: import.meta.env.DEV ? 'preview.contentful.com' : 'cdn.contentful.com',
+  });
