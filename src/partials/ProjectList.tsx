e6f406e442aad7f2976aaa56e29902bdcb45f56c
Tay Duncan <Tay.Duncan@autotrader.co.uk>
2023-07-27 09:38:49 +0100

    feat: projectList data fetched from github

--- src/partials/ProjectList.tsx
+++ src/partials/ProjectList.tsx
@@ -1,65 +1,41 @@
 import { ColorTags, GradientText, Project, Section, Tags } from '@/components';
 
-const ProjectList = () => (
-  <Section
-    title={
-      <>
-        Open Source <GradientText>Projects</GradientText>
-      </>
-    }
-  >
-    <div className="flex flex-col gap-6">
-      <Project
-        name="Project 1"
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-        bibendum. Nunc non posuere consectetur, justo erat semper enim, non
-        hendrerit dui odio id enim."
-        link="/"
-        img={{
-          src: '/assets/images/project-web-design.png',
-          alt: 'Project Web Design',
-        }}
-        category={
-          <>
-            <Tags color={ColorTags.FUCHSIA}>Astro.js</Tags>
-            <Tags color={ColorTags.LIME}>Web design</Tags>
-            <Tags color={ColorTags.SKY}>Tailwind.css</Tags>
-            <Tags color={ColorTags.ROSE}>TypeScript</Tags>
-          </>
-        }
-      />
-      <Project
-        name="Project 2"
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-        bibendum. Nunc non posuere consectetur, justo erat semper enim, non
-        hendrerit dui odio id enim."
-        link="/"
-        img={{ src: '/assets/images/project-fire.png', alt: 'Project Fire' }}
-        category={
-          <>
-            <Tags color={ColorTags.VIOLET}>Next.js</Tags>
-            <Tags color={ColorTags.EMERALD}>Blog</Tags>
-            <Tags color={ColorTags.YELLOW}>JavaScript</Tags>
-          </>
-        }
-      />
-      <Project
-        name="Project 3"
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-        bibendum. Nunc non posuere consectetur, justo erat semper enim, non
-        hendrerit dui odio id enim."
-        link="/"
-        img={{ src: '/assets/images/project-maps.png', alt: 'Project Maps' }}
-        category={
-          <>
-            <Tags color={ColorTags.FUCHSIA}>Astro.js</Tags>
-            <Tags color={ColorTags.INDIGO}>Bootstrap</Tags>
-            <Tags color={ColorTags.ROSE}>TypeScript</Tags>
-          </>
-        }
-      />
-    </div>
-  </Section>
+const data = await fetch('https://api.github.com/orgs/codingmugen/repos').then(
+  (response) => response.json()
 );
+
+const ProjectList = () => {
+  return (
+    <Section
+      title={
+        <>
+          Open Source <GradientText>Projects</GradientText>
+        </>
+      }
+    >
+      <div className="flex flex-col gap-6">
+        {data.map((project: any) => (
+          <Project
+            name={project.name}
+            description={project.description}
+            link={project.html_url}
+            img={{
+              src: '/assets/images/project-web-design.png',
+              alt: 'Project Web Design',
+            }}
+            category={
+              <>
+                <Tags color={ColorTags.FUCHSIA}>Astro.js</Tags>
+                <Tags color={ColorTags.LIME}>Web design</Tags>
+                <Tags color={ColorTags.SKY}>Tailwind.css</Tags>
+                <Tags color={ColorTags.ROSE}>TypeScript</Tags>
+              </>
+            }
+          />
+        ))}
+      </div>
+    </Section>
+  );
+};
 
 export { ProjectList };
