2596db82a16cb3d90922b37e60dc9a47cb3cdf2d
Tay Duncan <Tay.Duncan@autotrader.co.uk>
2023-07-27 08:16:33 +0100

    chore: optimize images

--- src/partials/Hero.tsx
+++ src/partials/Hero.tsx
@@ -23,40 +23,60 @@ const Hero = () => (
       avatar={
         <img
           className="h-80 w-80"
-          src="/assets/images/tashan.jpg"
+          src="/assets/images/tashan-min.jpg"
           alt="Avatar image"
           loading="lazy"
         />
       }
       socialButtons={
         <>
-          <a href="https://github.com/TashanDuncan">
+          <a
+            href="https://github.com/TashanDuncan"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <HeroSocial
-              src="/assets/images/github-icon.png"
+              src="/assets/images/github-icon-min.png"
               alt="Twitter icon"
             />
           </a>
-          <a href="https://twitter.com/CodingMugen">
+          <a
+            href="https://twitter.com/CodingMugen"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <HeroSocial
-              src="/assets/images/twitter-icon.png"
+              src="/assets/images/twitter-icon-min.png"
               alt="Twitter icon"
             />
           </a>
-          <a href="https://www.linkedin.com/in/tashanduncan/">
+          <a
+            href="https://www.linkedin.com/in/tashanduncan/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <HeroSocial
-              src="/assets/images/linkedin-icon.png"
+              src="/assets/images/linkedin-icon-min.png"
               alt="Linkedin icon"
             />
           </a>
-          <a href="https://www.youtube.com/channel/UCgjw8jmwtdjIGRERjBTt5hQ">
+          <a
+            href="https://www.youtube.com/channel/UCgjw8jmwtdjIGRERjBTt5hQ"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <HeroSocial
-              src="/assets/images/youtube-icon.png"
+              src="/assets/images/youtube-icon-min.png"
               alt="Youtube icon"
             />
           </a>
-          <a href="https://www.instagram.com/codingmugen">
+          <a
+            href="https://www.instagram.com/codingmugen"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <HeroSocial
-              src="/assets/images/instagram-icon.png"
+              src="/assets/images/instagram-icon-min.png"
               alt="Instagram icon"
             />
           </a>
